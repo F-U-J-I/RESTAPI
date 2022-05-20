696a2745b80e36b4e6fbced59045a8c690d21dd6
Keeper-of-honor <81534024+dmakger@users.noreply.github.com>
2022-05-21 02:55:25 +0300

    Create "Profile" model

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Advanced User"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_path = models.CharField(max_length=32)
+    avatar_url = models.ImageField(default="default.jpg")
+    wrapper_url = models.ImageField(blank=True)
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+def create_profile(sender, **kwargs):
+    """When a user is created, a profile is also created"""
+    if kwargs['created']:
+        profile = Profile.objects.create(user=kwargs['instance'])
+        profile.user_path = profile.pk
+        profile.save()
+
+
+post_save.connect(create_profile, sender=User)
