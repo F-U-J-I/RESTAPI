d341d514fd7cb30b855b1b22968e7d9a2a8db688
dmakger <dmakger@yandex.ru>
2022-05-21 22:05:45 +0300

    Create Register user. But, doesn't he see the path => http://localhost:8000/api/register/

---
+++ api/core/serializers.py
@@ -0,0 +1,50 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Profile
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    repeat_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'repeat_password')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        repeat_password = validated_data['repeat_password']
+
+        if password != repeat_password:
+            raise serializers.ValidationError({'password': 'Пароли не совпадают'})
+
+        user = User(username=username, email=email)
+        user.set_password(password)
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return profile
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def get_profile(self, user):
+        return ProfileSerializer(user.profile).data
--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import permissions, generics
+
+from .serializers import RegisterSerializer, ProfileSerializer
+
 
 # Create your views here.
+class RegisterView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        profile = serializer.save()
+        return Response({
+            'profile': ProfileSerializer(profile, context=self.get_serializer_context()).data,
+            'message': "Пользователь успешно создан"
+        })
