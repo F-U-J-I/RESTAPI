65354a4d103b275b8f8e001c63d42bf3198aeffc
Keeper-of-honor <81534024+dmakger@users.noreply.github.com>
2022-05-21 12:33:37 +0300

    Create "Course" and "CourseStatus" model

--- api/core/admin.py
+++ api/core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Profile, Subscription
+from .models import Profile, Subscription, CourseStatus, Course
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Subscription)
+
+admin.site.register(CourseStatus)
+admin.site.register(Course)
--- api/core/models.py
+++ api/core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+import datetime
 
 
 class Profile(models.Model):
     """Advanced User"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_path = models.CharField(max_length=32)
-    avatar_url = models.ImageField(default="default.jpg")
+    avatar_url = models.ImageField(default="profile-default.jpg")
     wrapper_url = models.ImageField(blank=True)
 
     def __str__(self):
@@ -26,8 +27,45 @@ post_save.connect(create_profile, sender=User)
 
 
 class Subscription(models.Model):
+    """
+    goal - the one who subscribed
+    subscriber - one who subscribes
+    """
     goal = models.ForeignKey(Profile, related_name="goal", on_delete=models.CASCADE)
     subscriber = models.ForeignKey(Profile, related_name="subscriber", on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.subscriber.user.username} => {self.goal.user.username}'
+
+
+class CourseStatus(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=175, blank=True)
+    price = models.IntegerField(blank=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    avatar_url = models.ImageField(default="course-default.svg")
+    duration_in_minutes = models.IntegerField(default=0)
+    rating = models.FloatField(default=0.)
+    members_amount = models.IntegerField(default=0)
+    max_progress_points = models.IntegerField(default=0)
+    status = models.ForeignKey(CourseStatus, null=True, on_delete=models.SET_NULL)
+    date_create = models.DateField(default=datetime.date.today)
+    course_path = models.CharField(max_length=32)
+
+
+def create_course(sender, **kwargs):
+    """When a user is created, a profile is also created"""
+    if kwargs['created']:
+        course = Course.objects.create(user=kwargs['instance'])
+        course.course_path = course.pk
+        course.save()
+
+
+post_save.connect(create_course, sender=Course)
